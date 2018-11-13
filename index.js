1cc46591c7663d175014c4b678f211ddb38a3923
Artem <avanglion59@gmail.com>
2018-11-13 12:27:53 +0200

    Update interface and logic

--- index.js
+++ index.js
@@ -1,25 +1,124 @@
-$(".submitter").on('click', function () {
-	let refferer = $(this).attr('data-ref');
-	if($(`input[name='${refferer}']:checked`).attr('data-right') == "") {
-		$(`p[data-ref=${refferer}]`).text("Угадали");
-		localStorage.setItem(refferer, 'passed');
-		redraw();
-	}
-	else {
-		$(`p[data-ref=${refferer}]`).text("Не угадали");
-	}
-});
-
-function redraw() {
-	for (let i of $('.timeline__date')) {
-		let refferer = $(i).attr('data-ref');
-		if (localStorage.getItem(refferer) == 'passed') {
-			$(i).css('background-color', 'green');
+var app = new Vue({
+	el: '#app',
+	mounted() {
+		if (localStorage.stage) {
+			this.stage = localStorage.stage;
 		}
-		// else {
-		// 	$(i).css('background-color', '#5c0120');
-		// }
-	}
-}
+	},
+	watch: {
+		stage(newStage) {
+			localStorage.stage = newStage;
+		}
+	},
+	computed: {
+		groupedCards() {
+			return _.chunk(this.cards, 4)
 
-redraw();
+		}
+	},
+	methods: {
+		checkAnswers(card) {
+			let rightVariants = card.test.variants.filter(variant => variant.right).map(variant => variant.text);
+			if (this.arraysEqual(rightVariants, card.test.chosen)) {
+				this.stage++;
+			}
+
+		},
+		arraysEqual(a, b) {
+			if (a === b) return true;
+			if (a == null || b == null) return false;
+			if (a.length != b.length) return false;
+
+			for (var i = 0; i < a.length; ++i) {
+				if (a[i] !== b[i]) return false;
+			}
+			return true;
+		}
+	},
+	data: {
+		stage: 0,
+		cards: [
+		{
+			title: "Card 1",
+			content: "На этой станции вы должны ознакомиться с основами прохождения квеста",
+			modalContent: "Тут будет что-то...",
+			secretWord: "Секретное слово 1",
+			test: {
+				type: "checkbox",
+				question: "Выберите правильный ответ: ",
+				chosen: [],
+				variants: [
+				{
+					text: "A",
+					right: true,
+				},
+				{
+					text: "B",
+					right: true,
+				},
+				{
+					text: "C",
+					right: true,
+				},
+				{
+					text: "D",
+					right: false,
+				},
+				{
+					text: "E",
+					right: false,
+				}
+				]
+			}
+		},
+		{
+			title: "Card 1",
+			content: "На этой станции вы должны ознакомиться с основами прохождения квеста",
+			modalContent: "Тут будет что-то...",
+			secretWord: "Секретное слово 2",
+			test: {
+				type: "radio",
+				question: "Выберите правильный ответ: ",
+				chosen: [],
+				variants: [
+				{
+					text: "A",
+					right: true,
+				},
+				{
+					text: "B",
+					right: false,
+				},
+				{
+					text: "C",
+					right: false,
+				},
+				{
+					text: "D",
+					right: false,
+				},
+				{
+					text: "E",
+					right: false,
+				}
+				]
+			}
+		},
+		{
+			title: "Card 1",
+			content: "На этой станции вы должны ознакомиться с основами прохождения квеста",
+			modalContent: "Тут будет что-то...",
+		},
+		{
+			title: "Card 1",
+			content: "На этой станции вы должны ознакомиться с основами прохождения квеста",
+			modalContent: "Тут будет что-то...",
+		},
+		{
+			title: "Card 1",
+			content: "На этой станции вы должны ознакомиться с основами прохождения квеста",
+			modalContent: "Тут будет что-то...",
+		}
+		]
+	}
+})
